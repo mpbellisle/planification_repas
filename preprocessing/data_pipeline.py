6f55d499e9f206521278783fd2388f231cc13cc1
Maxime Poulin-Bellisle <mpbellisle@gmail.com>
2021-04-18 14:11:38 -0400

    ptites modifs

--- preprocessing/data_pipeline.py
+++ preprocessing/data_pipeline.py
@@ -2,10 +2,10 @@ from preprocessing.data_cleaner import get_merge_df
 from preprocessing.categories_scraper import trouver_moment_sur_series, trouver_nationalite_sur_series
 
 
-recipes_path = "../data/recipes.csv"
-clean_recipes_path = "../data/clean_recipes.csv"
-out_path = "../data/recipes_out.pkl"
-nrows = 1000
+recipes_path = "data/recipes.csv"
+clean_recipes_path = "data/clean_recipes.csv"
+nrows = 200
+out_path = f"data/recipes_out_{nrows if nrows else 'full'}.pkl"
 
 if __name__ == "__main__":
     recipes = get_merge_df(clean_recipes_path, recipes_path).head(nrows)
