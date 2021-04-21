ed76b7d206a107edc8e9f37d6ccc542af80a673f
Maxime Poulin-Bellisle <mpbellisle@gmail.com>
2021-04-21 08:50:56 -0400

    2k run baby

--- preprocessing/data_pipeline.py
+++ preprocessing/data_pipeline.py
@@ -4,7 +4,7 @@ from preprocessing.categories_scraper import trouver_moment_sur_series, trouver_
 
 recipes_path = "data/recipes.csv"
 clean_recipes_path = "data/clean_recipes.csv"
-nrows = 500
+nrows = 2000
 out_path = f"data/recipes_out_{nrows if nrows else 'full'}.pkl"
 
 if __name__ == "__main__":
--- preprocessing/format_dzn.py
+++ preprocessing/format_dzn.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 from preprocessing.constant import NATIONALITES, ING_TO_REMOVE, BASE_ING_QTE_MAPPING, AVAILABLE_ING
 
-nrows = None
-processed_recipes_path = "data/recipes_out_200.pkl"
+nrows = 500
+processed_recipes_path = "data/recipes_out_500.pkl"
 dzn_out_path = f"data/data_recettes_out_{nrows if nrows else 'full'}.dzn"
 
 
